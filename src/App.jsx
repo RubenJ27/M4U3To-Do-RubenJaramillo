6b344fdccdca75f171a80dc46887cc933a835dd0
Ruben-Jaramillo <tiburones.tk@gmail.com>
2022-09-02 23:37:18 -0500

    Se creo el componente formulario

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import './App.css'
+import Form from './components/Form'
 import Header from './components/Header'
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <div className="container mx-auto mt-10">
     <Header />
     <div className="md:mx-36 my-1 md:flex">
+    <Form />
     </div>
     </div>
   )
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,7 +1,96 @@
+import { useState } from "react";
 
+const Form = () => {
 
-/* export const Form = () => {
+    const [user, setUser] = useState("");
+    const [phone, setPhone] = useState("");
+    const [email, setEmail] = useState("");
+    const [date, setDate] = useState("");
+
+    const [error, setError] = useState(false);
+
+    const handleSubmit = (e) => {
+      e.preventDefault();
+
+      if ([user, phone, email, date].includes("")) {
+        setError(true);
+        return;
+      }
+
+      setError(false);
+
+      /* Limpiar formulario */
+      setUser("");
+      setPhone("");
+      setEmail("");
+      setDate("");
+    } 
+
+ 
   return (
-    <div>Form</div>
-  )
-} */
+    <>
+    <div className="md:w-full h-full lg:w-1/2 mx-5 relative">
+      {/* Inicia el Formulario */}
+      <form
+        onSubmit={handleSubmit}
+        className="bg-white shadow-md rounded-lg py-7 px-5 mb-1 h-full relative"
+      >
+        <div className="mb-5">
+          <label
+            htmlFor="user"
+            className="block text-gray-700 uppercase font-bold"
+          >
+            Titulo
+          </label>
+          <input
+            id="user"
+            type="text"
+            placeholder="Nombres"
+            className="border-2 w-full p-2 mt-2 rounded-md placeholder-gray-400"
+            value={user}
+            onChange={(e) => setUser(e.target.value)}
+          />
+        </div>
+        
+        <div className="mb-5">
+          <label
+            htmlFor="fecha"
+            className="block text-gray-700 uppercase font-bold"
+          >
+            FECHA
+          </label>
+          <input
+            id="date"
+            type="date"
+            className="text-gray-700 border-2 w-full p-2 mt-2 rounded-md placeholder-gray-400"
+            value={date}
+            onChange={(e) => setDate(e.target.value)}
+          />
+        </div>
+
+        <div className="mb-5">
+          <label
+            htmlFor="fecha"
+            className="block text-gray-700 uppercase font-bold"
+          >
+            DESCRIPCION
+          </label>
+          <textarea name="" className="placeholder-gray-400 border-2 rounded-md pl-1 pt-1 text-gray-700" id="" cols="50" rows="5" placeholder="Descripcion de tarea"></textarea>
+        </div>
+
+
+        <input
+          type="submit"
+          className="bg-blue-600 w-full p-2 text-white uppercase font-bold rounded-full hover:bg-blue-700 transition-colors cursor-pointer"
+          value="Crear tarea"
+        />
+      </form>
+    </div>
+
+  </>
+
+
+  );
+}
+
+export default Form;
