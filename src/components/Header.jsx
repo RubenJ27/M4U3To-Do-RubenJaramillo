50b0455ad0bc2e61f54dd1680a2ec4c98397eea2
Ruben-Jaramillo <tiburones.tk@gmail.com>
2022-09-02 22:57:19 -0500

    Se creo el componente header de la aplicacion

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+function Header() {
+
+  return (
+      <h1 className="font-black text-5xl mb-8 text-center mx-auto md:w-2/3">Aplicacion <span className="text-green-800">TO-DO</span></h1>
+
+  )
+}
+
+export default Header
---
+++ src/components/TaskList.jsx
@@ -0,0 +1,7 @@
+
+
+/* export const TaskList = () => {
+  return (
+    <div>TaskList</div>
+  )
+} */
